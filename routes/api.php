b7b95613223fed4da37df5f491d6a241a1829433
Pelepoy <philipmarcel.maratas@gmail.com>
2025-01-24 16:58:00 +0800

    Add API resource routes for PostController

--- routes/api.php
+++ routes/api.php
@@ -1,15 +1,10 @@
 <?php
 
+use App\Http\Controllers\PostController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-// Route::get('/user', function (Request $request) {
-//     return $request->user();
-// })->middleware('auth:sanctum');
-
-Route::get('/', function (Request $request) {
-    return response()->json([
-        'message' => 'Hello World!',
-        'status' => 200
-    ]);
-});
+/**
+ * @Resource Routes
+ */
+Route::apiResource('posts', PostController::class);
