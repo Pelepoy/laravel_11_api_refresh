fdc47acc561ba3b7046aa7a5ef041ba609b10d28
Pelepoy <philipmarcel.maratas@gmail.com>
2025-01-29 18:23:46 +0800

    Add email verification routes and group authentication middleware

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,8 @@
 <?php
 
-use App\Http\Controllers\AuthController;
-use App\Http\Controllers\PostController;
+use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\EmailVerificationController;
+use App\Http\Controllers\Api\PostController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -12,7 +13,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
-Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
+
+Route::middleware(['auth:sanctum'])->group(function () {
+	Route::post('/email-verification', [EmailVerificationController::class, 'email_verification']);
+	Route::get('/resend-otp', [EmailVerificationController::class, 'resend_otp']);
+	Route::post('/logout', [AuthController::class, 'logout']);
+});
 
 /**
  * @Resource Routes
